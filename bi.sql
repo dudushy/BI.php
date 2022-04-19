88b9fa86cfbb9bae8acde23df564102769dcf717
dudushy <eduardotalarico1337@gmail.com>
2022-04-19 13:27:01 -0300

    Recreating database

--- bi.sql
+++ bi.sql
@@ -1,71 +1,21 @@
--- phpMyAdmin SQL Dump
--- version 4.8.5
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Apr 13, 2022 at 05:02 PM
--- Server version: 10.1.38-MariaDB
--- PHP Version: 5.6.40
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Database: `bi`
---
-
+-- database
 CREATE SCHEMA IF NOT EXISTS `bi` DEFAULT CHARACTER SET utf8 ;
 USE `bi` ;
 
+-- tb_group
+CREATE TABLE `bi`.`tb_group`(
+  `grp_id` INT NOT NULL,
+  `grp_name` VARCHAR(400) NOT NULL,
+  `grp_total_companies` INT NOT NULL,
+  PRIMARY KEY (`grp_id`)
+) ENGINE = InnoDB;
 
--- --------------------------------------------------------
+-- tb_company
+CREATE TABLE `bi`.`tb_company`(
+  `com_id` INT NOT NULL,
+  `com_name` VARCHAR(400) NOT NULL,
+  `grp_id` INT NOT NULL,
+  PRIMARY KEY (`com_id`)
+) ENGINE = InnoDB;
 
---
--- Table structure for table `tb_company`
---
-
-CREATE TABLE `tb_company` (
-  `com_id` int(11) NOT NULL,
-  `com_name` varchar(400) NOT NULL,
-  `com_group` int(11) NOT NULL -- foreign key?
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `tb_group`
---
-
-CREATE TABLE `tb_group` (
-  `grp_id` int(11) NOT NULL,
-  `grp_name` varchar(400) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tb_company`
---
-ALTER TABLE `tb_company`
-  ADD PRIMARY KEY (`com_id`);
-
---
--- Indexes for table `tb_group`
---
-ALTER TABLE `tb_group`
-  ADD PRIMARY KEY (`grp_id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+ALTER TABLE `tb_company` ADD CONSTRAINT `FOREIGN` FOREIGN KEY (`grp_id`) REFERENCES `tb_group`(`grp_id`) ON DELETE RESTRICT ON UPDATE RESTRICT;
